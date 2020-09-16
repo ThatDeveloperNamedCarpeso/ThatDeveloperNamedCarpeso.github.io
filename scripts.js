1b962ba5d83e1e8404bd432f036ff3fa98566661
ThatDeveloperNamedCarpeso <baloucarps@gmail.com>
2020-09-17 00:36:30 +0800

    Added some stuff!

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ window.addEventListener('load', event => {
 function _event() {
     var options = {
         strings: [
-            'sites',
-            'articles',
-            'software'
+            'sites.',
+            'articles.',
+            'software.'
         ],
         backDelay: 3000,
         typeSpeed: 100,
@@ -15,6 +15,14 @@ function _event() {
         smartBackspace: false,
         loop: true
     };
-
     var typed = new Typed('#change_text', options);
+
+    document.getElementById('darkmode').addEventListener('change', event => {
+        document.getElementsByTagName('body').item(0).classList.toggle('dark-mode');
+        if(document.getElementById('darkmode').checked == true) {
+            document.getElementById('toggle_text').innerText = "It's too dark!";
+        } else {
+            document.getElementById('toggle_text').innerText = "It's kinda bright!";
+        }
+    });
 }
