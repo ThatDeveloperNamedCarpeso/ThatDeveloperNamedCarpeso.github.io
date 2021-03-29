3309aa733c7a54ce22e264e7ed4e17e1e858a7d2
Baloucarps <Baloucarps@gmail.com>
2021-03-29 20:50:48 +0800

    Added my testimony. Final phase

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', event => {
     document.querySelector('.loader').classList.add('hide');
-})
+
+    document.querySelector('.exit').addEventListener('click', event => {
+        document.querySelector('.modal').classList.add('hide');
+    });
+
+    document.querySelector('.article-open').addEventListener('click', event => {
+        document.querySelector('.modal').classList.remove('hide');
+    });
+});
